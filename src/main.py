d445c1134d5237f91c0e0b22f4f63ec1b9dc747c
alexmzeier <61879578+alexmzeier@users.noreply.github.com>
2024-12-10 17:09:15 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,38 +2,47 @@ from appwrite.client import Client
 from appwrite.services.users import Users
 from appwrite.exception import AppwriteException
 import os
+import json
+from urllib.parse import parse_qs
 
 # This Appwrite function will be executed every time your function is triggered
 def main(context):
     # You can use the Appwrite SDK to interact with other services
     # For this example, we're using the Users service
+
+    userId = context.req.headers["x-appwrite-user-id"]
+    
+    data = json.loads(context.req.body)
+    label = data['label']
+    
     client = (
         Client()
         .set_endpoint(os.environ["APPWRITE_FUNCTION_API_ENDPOINT"])
         .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
         .set_key(context.req.headers["x-appwrite-key"])
     )
-    users = Users(client)
+    users_service = Users(client)
 
     try:
-        response = users.list()
-        # Log messages and errors to the Appwrite Console
-        # These logs won't be seen by your end users
-        context.log("Total users: " + str(response["total"]))
-    except AppwriteException as err:
-        context.error("Could not list users: " + repr(err))
-
-    # The req object contains the request data
-    if context.req.path == "/ping":
-        # Use res object to respond with text(), json(), or binary()
-        # Don't forget to return a response!
-        return context.res.text("Pong")
-
-    return context.res.json(
-        {
-            "motto": "Build like a team of hundreds_",
-            "learn": "https://appwrite.io/docs",
-            "connect": "https://appwrite.io/discord",
-            "getInspired": "https://builtwith.appwrite.io",
+        # Erstelle einen neuen Benutzer
+        users_service.update_labels(
+            userId,
+            [ label ]
+        )
+                
+        response = {
+            "status": "ok",
+            "message": "label updated"
         }
-    )
+    except Exception as e:
+        response = {
+            "status": "error",
+            "message": str(e)
+        }
+        
+    response = {
+            "status": "ok",
+            "message": "label updated"
+        }
+    
+    return context.res.json(response)
